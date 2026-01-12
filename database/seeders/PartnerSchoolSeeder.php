b9db6a81eef45dfa36da855c7773ef68667a121b
Farid Dihan Nahdi <dihannahdii@gmail.com>
2026-01-13 06:44:24 +0700

    refactor: remove sample partner schools data
    - Clear PartnerSchoolSeeder content
    - Remove MA Al-Hikmah, MA Nurul Huda, MA Darul Ulum, MA Raudlatul Muta'allimin

--- database/seeders/PartnerSchoolSeeder.php
+++ database/seeders/PartnerSchoolSeeder.php
@@ -12,51 +12,6 @@ class PartnerSchoolSeeder extends Seeder
      */
     public function run(): void
     {
-        $schools = [
-            [
-                'name' => 'MA Al-Hikmah Yogyakarta',
-                'address' => 'Jl. Parangtritis No.123, Yogyakarta',
-                'contact_person' => 'Ustadz Ahmad Fauzi',
-                'phone' => '0274-123456',
-                'email' => 'ma.alhikmah@gmail.com',
-                'partnership_date' => '2024-01-15',
-                'logo' => null,
-                'is_active' => true,
-            ],
-            [
-                'name' => 'MA Nurul Huda Sleman',
-                'address' => 'Jl. Kaliurang Km.12, Sleman',
-                'contact_person' => 'Ustadzah Siti Aminah',
-                'phone' => '0274-234567',
-                'email' => 'nurulhuda.sleman@gmail.com',
-                'partnership_date' => '2024-03-20',
-                'logo' => null,
-                'is_active' => true,
-            ],
-            [
-                'name' => 'MA Darul Ulum Bantul',
-                'address' => 'Jl. Imogiri Timur Km.8, Bantul',
-                'contact_person' => 'Ustadz Muhammad Amin',
-                'phone' => '0274-345678',
-                'email' => 'darululum.bantul@gmail.com',
-                'partnership_date' => '2024-06-10',
-                'logo' => null,
-                'is_active' => true,
-            ],
-            [
-                'name' => 'MA Raudlatul Muta\'allimin',
-                'address' => 'Jl. Wates Km.5, Yogyakarta',
-                'contact_person' => 'Ustadzah Fatimah Zahra',
-                'phone' => '0274-456789',
-                'email' => 'raudlatul.mutaallimin@gmail.com',
-                'partnership_date' => '2024-08-05',
-                'logo' => null,
-                'is_active' => true,
-            ],
-        ];
-
-        foreach ($schools as $school) {
-            \App\Models\PartnerSchool::create($school);
-        }
+        // Partner schools section removed as per request
     }
 }
