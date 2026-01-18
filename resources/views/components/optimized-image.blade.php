ef60bdd960976f226e9136fb21cb4527fae5e6cd
Farid Dihan Nahdi <dihannahdii@gmail.com>
2026-01-18 14:23:58 +0700

    fix: fallback to original image if webp doesn't exist
    - Update optimized-image component to check if webp exists
    - Use original JPG/PNG if webp version not found
    - Fixes PKM Pesantren 2025 photos not displaying

--- resources/views/components/optimized-image.blade.php
+++ resources/views/components/optimized-image.blade.php
@@ -1,8 +1,14 @@
 @props(['src', 'alt', 'class' => '', 'loading' => 'lazy'])
 
 @php
-    // All images are now WebP only
+    // Try WebP first, fallback to original if WebP doesn't exist
     $webpPath = preg_replace('/\.(jpe?g|png|gif)$/i', '.webp', $src);
+    $publicPath = public_path($webpPath);
+    
+    // If webp doesn't exist, use original image
+    if (!file_exists($publicPath)) {
+        $webpPath = $src;
+    }
 @endphp
 
 <img 
